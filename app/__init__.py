70e7c5039b47eeccc475d914a81adb0c302816e0
Katrin Valge <katrin.valge@mail.ee>
2023-07-02 18:10:05 +0300

    Updated order.

--- app/__init__.py
+++ app/__init__.py
@@ -13,14 +13,14 @@ def create_app(config_class=Config):
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
-    from app.tasks.job import bp as job_bp
-    app.register_blueprint(job_bp, url_prefix='/job')
-
     from app.tasks.bills import bp as bills_bp
     app.register_blueprint(bills_bp, url_prefix='/bills')
 
     from app.tasks.entertainment import bp as entertainment_bp
     app.register_blueprint(entertainment_bp, url_prefix='/entertainment')
+
+    from app.tasks.job import bp as job_bp
+    app.register_blueprint(job_bp, url_prefix='/job')
 
     from app.tasks.studies import bp as studies_bp
     app.register_blueprint(studies_bp, url_prefix='/studies')
