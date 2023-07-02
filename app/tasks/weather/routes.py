c5243347c94c6efba26b4d6ac11d80564414f6a3
Katrin Valge <katrin.valge@mail.ee>
2023-07-02 19:20:13 +0300

    Added route to beach.html.

--- app/tasks/weather/routes.py
+++ app/tasks/weather/routes.py
@@ -5,3 +5,8 @@ from app.tasks.weather import bp
 @bp.route('/')
 def index():
     return render_template('weather/index.html')
+
+
+@bp.route('/beach/')
+def categories():
+    return render_template('weather/beach.html')
