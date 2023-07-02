c58ed61f386ac3b6d3deb1282e75a4c0a0ea7c55
Katrin Valge <katrin.valge@mail.ee>
2023-07-02 22:48:12 +0300

    Added functionality to add new job info.

--- app/tasks/job/routes.py
+++ app/tasks/job/routes.py
@@ -1,4 +1,7 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+
+from app import db
+from app.models.job import Job
 from app.tasks.job import bp
 
 
@@ -7,6 +10,12 @@ def index():
     return render_template('job/index.html')
 
 
-@bp.route('/new/')
+@bp.route('/new/', methods=('GET', 'POST'))
 def categories():
+    if request.method == 'POST':
+        new_question = Job(title=request.form['title'],
+                           content=request.form['content'])
+        db.session.add(new_question)
+        db.session.commit()
+        return redirect(url_for('job.index'))
     return render_template('job/new-job.html')
