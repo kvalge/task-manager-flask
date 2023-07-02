fca670f7ff0a6f72713729a21d7d4606938bbf9b
Katrin Valge <katrin.valge@mail.ee>
2023-07-02 19:32:40 +0300

    Created extensions.py to import the SQLAlchemy() class. __init__.py: integrated SQLAlchemy with the Flask application.

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,17 @@ from flask import Flask
 
 from config import Config
 
+from app.extensions import db
+
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # Initialize Flask extensions here
+    # Initializing Flask extensions
+    db.init_app(app)
 
-    # Register blueprints here
+    # Registering blueprints
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
