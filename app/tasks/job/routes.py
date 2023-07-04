1dd48d3250a73ca334eb757559852de359311cf8
Katrin Valge <katrin.valge@mail.ee>
2023-07-04 21:13:15 +0300

    Created get job by id and update job functionalities.

--- app/tasks/job/routes.py
+++ app/tasks/job/routes.py
@@ -14,12 +14,27 @@ def index():
 @bp.route('/new/', methods=('GET', 'POST'))
 def new():
     if request.method == 'POST':
-        new_question = Job(title=request.form['title'],
-                           content=request.form['content'])
-        db.session.add(new_question)
+        new_job = Job(title=request.form['title'],
+                      content=request.form['content'])
+        db.session.add(new_job)
         db.session.commit()
         return redirect(url_for('job.index'))
     return render_template('job/new-job.html')
+
+
+@bp.route('/edit/<id>', methods=['GET', 'POST'])
+def get_job_by_id_for_edit(id):
+    job = Job.query.filter_by(id=id).first()
+    return render_template('job/edit-job.html', job=job)
+
+
+@bp.route('/update/<id>', methods=['GET', 'POST'])
+def update(id):
+    job = Job.query.filter_by(id=id).first()
+    job.title = request.form["title"]
+    job.content = request.form["content"]
+    db.session.commit()
+    return index()
 
 
 @bp.route('/delete/<id>', methods=['GET', 'POST'])
