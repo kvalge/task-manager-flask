20d3a533a905fe585e8655eec2618948a8c5ee59
Katrin Valge <katrin.valge@mail.ee>
2023-07-04 22:14:22 +0300

    Created functionality to update and delete study program.

--- app/tasks/studies/routes.py
+++ app/tasks/studies/routes.py
@@ -23,6 +23,25 @@ def new():
     return render_template('studies/new_program.html')
 
 
-@bp.route('/university/')
-def categories():
-    return render_template('studies/university.html')
+@bp.route('/edit/<id>', methods=['GET', 'POST'])
+def get_program_by_id_for_edit(id):
+    program = Program.query.filter_by(id=id).first()
+    return render_template('studies/edit-program.html', program=program)
+
+
+@bp.route('/update/<id>', methods=['GET', 'POST'])
+def update(id):
+    program = Program.query.filter_by(id=id).first()
+    program.title = request.form["title"]
+    program.organization = request.form["organization"]
+    program.description = request.form["description"]
+    db.session.commit()
+    return index()
+
+
+@bp.route('/delete/<id>', methods=['GET', 'POST'])
+def delete(id):
+    program = Program.query.filter_by(id=id).first()
+    db.session.delete(program)
+    db.session.commit()
+    return index()
