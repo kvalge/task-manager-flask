4c93b50c71615247f5e38e8f2eece4608bec63ae
Katrin Valge <katrin.valge@mail.ee>
2023-07-04 22:01:21 +0300

    Created functionality to add a new study program.

--- app/tasks/studies/routes.py
+++ app/tasks/studies/routes.py
@@ -1,10 +1,25 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+
+from app import db
+from app.models.study_program import Program
 from app.tasks.studies import bp
 
 
 @bp.route('/')
 def index():
     return render_template('studies/index.html')
+
+
+@bp.route('/new/', methods=('GET', 'POST'))
+def new():
+    if request.method == 'POST':
+        new_program = Program(title=request.form['title'],
+                              organization=request.form['organization'],
+                              description=request.form['description'])
+        db.session.add(new_program)
+        db.session.commit()
+        return redirect(url_for('studies.index'))
+    return render_template('studies/new_program.html')
 
 
 @bp.route('/university/')
