deef6685eea05d9437e597a632546b72a8c7d335
Katrin Valge <katrin.valge@mail.ee>
2023-07-04 22:39:26 +0300

    Created functionalities to add, return, edit and delete bills.

--- app/tasks/bills/routes.py
+++ app/tasks/bills/routes.py
@@ -1,12 +1,47 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+
+from app import db
+from app.models.bill import Bill
 from app.tasks.bills import bp
 
 
 @bp.route('/')
 def index():
-    return render_template('bills/index.html')
+    bills = Bill.query.all()
+    return render_template('bills/index.html', bills=bills)
 
 
-@bp.route('/mobile/')
-def categories():
-    return render_template('bills/mobile.html')
+@bp.route('/new/', methods=('GET', 'POST'))
+def new():
+    if request.method == 'POST':
+        new_bill = Bill(service=request.form['service'],
+                        period=request.form['period'],
+                        price=request.form['price'])
+        db.session.add(new_bill)
+        db.session.commit()
+        return redirect(url_for('bills.index'))
+    return render_template('bills/new-bill.html')
+
+
+@bp.route('/edit/<id>', methods=['GET', 'POST'])
+def get_bill_by_id_for_edit(id):
+    bill = Bill.query.filter_by(id=id).first()
+    return render_template('bills/edit-bill.html', bill=bill)
+
+
+@bp.route('/update/<id>', methods=['GET', 'POST'])
+def update(id):
+    bill = Bill.query.filter_by(id=id).first()
+    bill.service = request.form["service"]
+    bill.period = request.form["period"]
+    bill.price = request.form["price"]
+    db.session.commit()
+    return index()
+
+
+@bp.route('/delete/<id>', methods=['GET', 'POST'])
+def delete(id):
+    bill = Bill.query.filter_by(id=id).first()
+    db.session.delete(bill)
+    db.session.commit()
+    return index()
